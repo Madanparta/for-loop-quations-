7e586502eb6904adce29c9d6bfa95eb7c9a672f5
madan k <partagowda15@gmil.com>
2022-06-17 21:10:46 +0530

    six quations are using while and for loop

--- abc.js
+++ abc.js
@@ -70,6 +70,66 @@
 // let p = 1;
 // for(let i = 1; i <=a; i++){
 //     p*=i;
+//     console.log(p);
 // }
-// console.log(p);
 
+
+
+// print all the digit all numbers...
+
+// let a = process.argv[2];
+// for(let i = a; i>0; i/=10 ){
+//     console.log(i%10);
+// }
+
+
+// printing all fibonacci series usin for loop..
+
+// let x = process.argv[2];
+// let a = 1;
+// let b = 1;
+// let p = 0;
+// for(let i = 1; i<x; i++){
+//     console.log(a);
+//     let p = a+b;
+//     a=b;
+//     b=p;
+// }
+
+
+// using while loop...
+// let a = process.argv[2];
+
+// let p = 1;
+// let i = 1;
+// while(i<=a){
+//     console.log(p*=i);
+//     i++;
+// }
+
+//printing all fibonacci series usin .. using while loop.
+
+// let a = process.argv[2];
+// let x = 1;
+// let y = 1;
+// let p = 0;
+
+// let i = 1;
+// while(i<a){
+//     console.log(x);
+//     let p = x + y;
+//     x = y;
+//     y = p;
+//     i++;
+
+// }
+
+
+// print all digits of a number using while loop..
+
+// let a = process.argv[2];
+// let i = a;
+// while(i>0){
+//     console.log(i%10);
+//     i/=10;
+// }
