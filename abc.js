49884f6b42fc5838d4d64e18c0216f69226fd838
madan k <partagowda15@gmil.com>
2022-06-17 19:50:55 +0530

    for loop

---
+++ abc.js
@@ -0,0 +1,22 @@
+// let a = process.argv[2];
+
+// using a loop print all natural number..
+
+// let a = process.argv[2];
+// for (let i = 1; i <= a; i++ ){
+//     console.log(i);
+// }
+
+// let a = process.argv[2];
+// for ( let i = 1 ; i <= a; i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+
+let a = process.argv[2];
+for(let i = a ; i>0; i--){
+    console.log(i);
+}
+
