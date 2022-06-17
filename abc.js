bf64b7936e0b0c3f26a734b040c78e8712fe6cfe
madan k <partagowda15@gmil.com>
2022-06-17 20:11:57 +0530

    using while and for loop

--- abc.js
+++ abc.js
@@ -7,6 +7,8 @@
 //     console.log(i);
 // }
 
+// printing even numbers..
+
 // let a = process.argv[2];
 // for ( let i = 1 ; i <= a; i++){
 //     if(i%2==0){
@@ -15,8 +17,59 @@
 // }
 
 
-let a = process.argv[2];
-for(let i = a ; i>0; i--){
-    console.log(i);
-}
+// reverse usin for loop...
+// let a = process.argv[2];
+// for(let i = a ; i>0; i--){
+//     console.log(i);
+// }
+
+
+// ..................................................................................
+// using whilw loop...
+// print all natural number using while loop
+
+
+
+// let a = process.argv[2];
+
+// let i = 1;
+// while(i <= a){
+//     console.log(i);
+//     i++;
+// }
+
+// printing even numbers..
+// let a = process.argv[2];
+
+// let i = 1;
+// while(i<=a){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+
+
+// reverse usin while loop...
+// let a = process.argv[2];
+
+// let i = a;
+// while(i>=0){
+//     console.log(i);
+//     i--;
+// }
+
+
+
+// ............................................................
+
+// for loop 
+// factorial numberss...
+// let a = process.argv[2];
+// let p = 1;
+// for(let i = 1; i <=a; i++){
+//     p*=i;
+// }
+// console.log(p);
 
